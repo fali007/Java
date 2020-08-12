5e2a9da43d7e28318acfa6af11d7143537c1dcc8
felix george <felixgeorge@felixs-MacBook-Air.local>
2020-08-12 22:54:45 +0530

    second

--- second/src/Main.java
+++ second/src/Main.java
@@ -1,17 +1,21 @@
 public class Main {
     public static void main(String[] args){
-//        calculateScore(true,800,5,100);
-//        calculateScore(false,200,3,100);
-//        checkNumber(7);
-//        checkNumber(-4);
-//        checkNumber(0);
-//        checkNumber(2,4);
-//        swith a=new swith();
-//        a.swith(1);
+        calculateScore(true,800,5,100);
+        calculateScore(false,200,3,100);
+        checkNumber(7);
+        checkNumber(-4);
+        checkNumber(0);
+        checkNumber(2,4);
+        swith a=new swith();
+        a.swith(1);
         loop lop =new loop();
         lop.loop(5);
         lop.whiile(5);
         lop.do_while(7);
+        parsingFromString s=new parsingFromString();
+        s.parse("100");
+
+
 
     }
     public static void calculateScore(boolean b,int score, int level,int bonus){
---
+++ second/src/parsingFromString.java
@@ -0,0 +1,6 @@
+public class parsingFromString {
+    public static void parse(String s){
+        int number=Integer.parseInt(s);
+        System.out.println("The number times 2 is : " + 2 * number);
+    }
+}
