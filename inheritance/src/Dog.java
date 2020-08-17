bc36014b4b9419d70c94bb2e712c60ca2d0eabda
felix george <felixgeorge@felixs-MacBook-Air.local>
2020-08-17 21:05:08 +0530

    message

---
+++ inheritance/src/Dog.java
@@ -0,0 +1,9 @@
+public class Dog extends Animal{
+    public Dog(String name, int size, int weight, String coat, int eyes, int legs, int tail) {
+        super(name, 1, 1, size, weight);
+    }
+    private int eyes;
+    private int legs;
+    private int tail;
+    private String coat;
+}
